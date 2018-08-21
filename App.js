7b2f208f4af3cb7676af22d7d3bc84525b59a8e8
taejune park <ptj1554@gmail.com>
2018-08-22 00:15:58 +0900

    getting the weather(first page)

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image, ActivityIndicator} from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>weather</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    state = {
+        isLoaded: false
+    };
+
+    render() {
+        const {isLoaded} = this.state;
+        return (
+            <View style={styles.container}>
+                {isLoaded ? null : (
+                    <View style={styles.loading}>
+                        <Text style={styles.loadingText}>Getting the weather</Text>
+                    </View>
+                )}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+
+    },
+    loading: {
+        flex: 1,
+        backgroundColor: "#FDF6AA",
+        justifyContent:"flex-end",
+
+
+    },
+    loadingText:{
+        fontSize:40,
+        marginBottom:100,
+        paddingLeft:15
+    }
 });
